a21823f696d50b0183fc0094b0735f3ae06a2249
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-12-07 13:42:15 +0100

    Edit heredoc welcome message

---
+++ tic_tac_toe.rb
@@ -0,0 +1,18 @@
+puts <<~WELCOME
+Welcome to Tic-Tac-Toe!
+
+A game played taking your turn marking the spaces in a three-by-three grid with X or O. 
+The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner: you can’t place a token on a square that’s already taken.
+
+Please, in placing your move consider a board with squares identified as follows:
+
+   1 | 2 | 3
+  ---+---+---
+   4 | 5 | 6
+  ---+---+---
+   7 | 8 | 9
+WELCOME
+
+class Tic_Tac_Toe
+  
+end
